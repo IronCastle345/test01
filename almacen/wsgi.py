725a0a5575174807827eed4eefb2747b5729bb00
IronCastle345 <85468705+IronCastle345@users.noreply.github.com>
2025-04-04 20:13:27 -0600

    Update wsgi.py

--- almacen/wsgi.py
+++ almacen/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'almacen.settings')
+path = '/home/IronCastle345/test01'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'almacen.settings'
 
 application = get_wsgi_application()
