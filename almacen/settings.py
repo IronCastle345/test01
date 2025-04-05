4428256b5572754991de85af256e5a3a40a1ee9a
IronCastle345 <85468705+IronCastle345@users.noreply.github.com>
2025-04-04 20:00:17 -0600

    Update settings.py

--- almacen/settings.py
+++ almacen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jm3xxwzzgiv_c3=p@8p8#^)@af=z1kgd&w!!!_^$iw7#+&e+0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
